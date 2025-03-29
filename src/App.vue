00deaece4ce90888c7c2d2a6524ffb00917cbff3
thesnakebite <alvaro@thesnakebite.es>
2025-03-29 22:57:28 +0100

    installing tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-    <h1>Hello Pinia</h1>
+    <div class="min-h-screen bg-gradient-to-br from-blue-500 to-purple-500 flex items-center justify-center">
+        <h1 class="text-3xl font-bold text-zinc-200">Hello Pinia</h1>
+    </div>
 </template>
 
 <style scoped>
