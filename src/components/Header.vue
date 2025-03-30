18938ed145282cfc710f30e44e979070c8f0dc5f
thesnakebite <alvaro@thesnakebite.es>
2025-03-30 13:19:31 +0200

    how to consume a store in Views or Components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,12 @@
 <script setup>
     import { computed } from 'vue'
     import { RouterLink, useRoute } from 'vue-router'
+    import { useBebidasStore } from '@/stores/bebidas'
 
     const route = useRoute()
+    const store = useBebidasStore()
+    console.log(store.categorias)
+    
 
     const paginaInicio = computed( () => route.name === 'inicio')
 </script>
