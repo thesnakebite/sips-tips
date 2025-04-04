2b298e0f34a6630e12897f1ae1d7fdd8e3ae745a
thesnakebite <alvaro@thesnakebite.es>
2025-04-04 19:40:21 +0200

    storing favorites in localStorage

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import { defineStore } from 'pinia'
 import { useBebidasStore } from './bebidas'
 
@@ -7,10 +7,18 @@ export const useFavoritosStore = defineStore('favoritos', () => {
     const bebida = useBebidasStore()
     const favoritos = ref([])
 
-    const handleClickFavorito = () => {
+    const sincronizarLocalStorage = () => {
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
+    }
 
+    watch(favoritos, () => {
+        sincronizarLocalStorage()
+    }, {
+        deep: true
+    })
+
+    const handleClickFavorito = () => {
         favoritos.value.push(bebida.receta)
-        
     }
 
     return {
