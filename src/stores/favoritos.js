fa4394c2bf8eec256ce5c3e3a17eed3f1fbc1d33
thesnakebite <alvaro@thesnakebite.es>
2025-04-18 21:08:23 +0200

    resetting the state in pinia

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -26,6 +26,10 @@ export const useFavoritosStore = defineStore('favoritos', () => {
 
         notificaciones.mostrar = true
         notificaciones.texto = 'Se agregó a favoritos'
+
+        setTimeout( () => {
+            notificaciones.$reset()
+        }, 3000)
     }
 
     function eliminarFavoritos() {
--- src/stores/notificaciones.js
+++ src/stores/notificaciones.js
@@ -2,14 +2,20 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useNotificacionesStore = defineStore('notificacion', () => {
-    const texto = ''
+    const texto = ref('')
     const error = ref(false)
     const mostrar = ref(false)
 
+    function $reset() {
+        texto.value = ''
+        error.value = false
+        mostrar.value = false
+    }
 
     return {
         texto,
         error,
         mostrar,
+        $reset,
     }
 })
