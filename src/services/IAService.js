ef452b5496cca89868ad98f97e049c55f2b269c8
thesnakebite <alvaro@thesnakebite.es>
2025-04-20 22:14:18 +0200

    creating the service to interact with AI

---
+++ src/services/IAService.js
@@ -0,0 +1,12 @@
+import { openrouter } from '@/lib/ia'
+import { streamText } from 'ai'
+
+export default {
+    async generarReceta(prompt) {
+        const resultado = streamText({
+            model:
+            prompt:
+        })
+        
+    }
+}
--- src/stores/ia.js
+++ src/stores/ia.js
@@ -1,12 +1,13 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import IAService, {} from '@/services/IAService'
 
 export const useIAStore = defineStore('ia', () => {
     const prompt = ref('')
     const respuesta = ref('')
 
     async function generarReceta() {
-        console.log('Desde generar receta')
+        await IAService.generarReceta(prompt.value) 
     }
 
     return {
