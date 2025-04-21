07152ca99bf1733b5b5fadc3f421e4afb2d9c0ed
thesnakebite <alvaro@thesnakebite.es>
2025-04-21 09:07:11 +0200

    choosing the model to consult and sending the request

--- src/services/IAService.js
+++ src/services/IAService.js
@@ -4,9 +4,10 @@ import { streamText } from 'ai'
 export default {
     async generarReceta(prompt) {
         const resultado = streamText({
-            model:
-            prompt:
+            model: openrouter('meta-llama/llama-4-scout:free'),
+            prompt
         })
         
+        return resultado.textStream
     }
 }
--- src/stores/ia.js
+++ src/stores/ia.js
@@ -1,13 +1,17 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import IAService, {} from '@/services/IAService'
+import IAService from '@/services/IAService'
 
 export const useIAStore = defineStore('ia', () => {
     const prompt = ref('')
     const respuesta = ref('')
 
     async function generarReceta() {
-        await IAService.generarReceta(prompt.value) 
+        const resultado = await IAService.generarReceta(prompt.value)
+        
+        for await(const texto of resultado) {
+            console.log(texto)
+        }
     }
 
     return {
